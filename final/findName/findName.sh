2bed89d7e621bf56c25a7dce9181bc32daf6b199
justinba1010 <justin.baum@ninefx.com>
2018-11-21 16:59:18 -0500

    Final

--- final/findName/findName.sh
+++ final/findName/findName.sh
@@ -8,5 +8,21 @@ if [ "$#" -ne 1 ]; then
 	exit
 fi
 
-# Can't access anything else rn
-echo $1
+FILE=/acct/common/CSCE215-Fall18
+STUDENT_ID=$1
+
+for line in $(cat $FILE); do 
+	LINE_ID=$(echo $line | cut -d',' -f4)
+	if [ "$LINE_ID" = "$STUDENT_ID" ]; then
+		FIRST=$(echo $line | cut -d',' -f1)
+		LAST=$(echo $line | cut -d ',' -f3)
+		MI=$(echo $line | cut -d ',' -f2)
+		echo $FIRST $MI  $LAST
+		F="true"
+	fi
+done;
+
+if [ -z $F ]; then
+	echo "User is not in CSCE215 this semester or doesn't exist."
+fi
+
